f6b91692d0f6955008d287e618b01614c7c888b1
Ahmed Sereta <seretaahmed18@gmail.com>
2024-05-25 23:55:18 +0300

    Added file based routing

--- app/index.jsx
+++ app/index.jsx
@@ -1,23 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
-import { Image, StyleSheet, Text, Platform, View } from 'react-native';
+import { Image, Text, Platform, View } from 'react-native';
 import { Link } from 'expo-router'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Paramax</Text>
+    <View className="flex-1 bg-white items-center justify-center">
+      <Text className="text-4xl f">Paramax</Text>
       <StatusBar style='auto'/>
       <Link href={'/profile'} style={{ color: 'blue' }}>Go to profile</Link>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    paddingTop: 40,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
